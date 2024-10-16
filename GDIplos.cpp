3d899b7d31e6dc395a6327db378f61c8d658184d
nwiart <nwiart@gaming.tech>
2024-10-16 13:17:38 +0200

    Minimum

--- GDIplos.cpp
+++ GDIplos.cpp
@@ -79,75 +79,77 @@ Gdiplus::Bitmap* GdiPlusManager::ResizeImage(Gdiplus::Bitmap* bmp, int newWidth,
 
 void GdiPlusManager::ApplyBlur(int radius)
 {
-    if (loadedImage) {
-        int width = loadedImage->GetWidth();
-        int height = loadedImage->GetHeight();
-
-        Gdiplus::Bitmap* blurredImage = new Gdiplus::Bitmap(width, height, loadedImage->GetPixelFormat());
-
-        for (int y = radius; y < height - radius; y++) {
-            for (int x = radius; x < width - radius; x++) {
-                int r = 0, g = 0, b = 0, count = 0;
-
-                for (int ky = -radius; ky <= radius; ky++) {
-                    for (int kx = -radius; kx <= radius; kx++) {
-                        Gdiplus::Color color;
-                        loadedImage->GetPixel(x + kx, y + ky, &color);
-
-                        r += color.GetRed();
-                        g += color.GetGreen();
-                        b += color.GetBlue();
-                        count++;
-                    }
-                }
-
-                r /= count;
-                g /= count;
-                b /= count;
-
-                Gdiplus::Color avgColor(r, g, b);
-                blurredImage->SetPixel(x, y, avgColor);
-            }
-        }
-
-        delete loadedImage;
-        loadedImage = blurredImage;
-
-        Application::log(("Applied manual blur with radius: " + std::to_string(radius)).c_str());
+    if (!loadedImage) {
+        return;
     }
+
+    int width = loadedImage->GetWidth();
+    int height = loadedImage->GetHeight();
+
+    Gdiplus::Bitmap* blurredImage = new Gdiplus::Bitmap(width, height, loadedImage->GetPixelFormat());
+
+    for (int y = radius; y < height - radius; y++) {
+        for (int x = radius; x < width - radius; x++) {
+            int r = 0, g = 0, b = 0, count = 0;
+
+            for (int ky = -radius; ky <= radius; ky++) {
+                for (int kx = -radius; kx <= radius; kx++) {
+                    Gdiplus::Color color;
+                    loadedImage->GetPixel(x + kx, y + ky, &color);
+
+                    r += color.GetRed();
+                    g += color.GetGreen();
+                    b += color.GetBlue();
+                    count++;
+                }
+            }
+
+            r /= count;
+            g /= count;
+            b /= count;
+
+            Gdiplus::Color avgColor(r, g, b);
+            blurredImage->SetPixel(x, y, avgColor);
+        }
+    }
+
+    delete loadedImage;
+    loadedImage = blurredImage;
+
+    Application::log(("Applied manual blur with radius: " + std::to_string(radius)).c_str());
 }
 
 void GdiPlusManager::ApplySepia()
 {
-    if (loadedImage) {
-        int width = loadedImage->GetWidth();
-        int height = loadedImage->GetHeight();
-
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                Gdiplus::Color color;
-                loadedImage->GetPixel(x, y, &color);
-
-                int r = color.GetRed();
-                int g = color.GetGreen();
-                int b = color.GetBlue();
-
-                int tr = (int)(0.393 * r + 0.769 * g + 0.189 * b);
-                int tg = (int)(0.349 * r + 0.686 * g + 0.168 * b);
-                int tb = (int)(0.272 * r + 0.534 * g + 0.131 * b);
-
-                tr = (tr > 255) ? 255 : tr;
-                tg = (tg > 255) ? 255 : tg;
-                tb = (tb > 255) ? 255 : tb;
-
-                Gdiplus::Color sepiaColor(tr, tg, tb);
-
-                loadedImage->SetPixel(x, y, sepiaColor);
-            }
-        }
-
-        Application::log("Applied sepia filter to the image.");
+    if (!loadedImage) {
+        return;
     }
+
+    int width = loadedImage->GetWidth();
+    int height = loadedImage->GetHeight();
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            Gdiplus::Color color;
+            loadedImage->GetPixel(x, y, &color);
+
+            int r = color.GetRed();
+            int g = color.GetGreen();
+            int b = color.GetBlue();
+
+            int tr = (int)(0.393 * r + 0.769 * g + 0.189 * b);
+            int tg = (int)(0.349 * r + 0.686 * g + 0.168 * b);
+            int tb = (int)(0.272 * r + 0.534 * g + 0.131 * b);
+
+            tr = (tr > 255) ? 255 : tr;
+            tg = (tg > 255) ? 255 : tg;
+            tb = (tb > 255) ? 255 : tb;
+
+            Gdiplus::Color sepiaColor(tr, tg, tb);
+
+            loadedImage->SetPixel(x, y, sepiaColor);
+        }
+    }
+
+    Application::log("Applied sepia filter to the image.");
 }
-
-
